eeb8c7f36cd43d1d0cb4a7e240a53e01b5f1f613
shahrizoda <shahrizodadeveloping@gmail.com>
2022-03-12 23:46:05 +0500

    intro

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,22 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+new Swiper(".intro__swiper", {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: 15,
+    navigation: {
+      nextEl: ".intro__button-next",
+      prevEl: ".intro__button-prev",
+    },
+    breakpoints: {
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
